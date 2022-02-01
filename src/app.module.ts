4eb6d252391ed03e64b61847fea7762c31c41256
Denyse <mdenyse15@gmail.com>
2022-02-01 11:27:34 +0200

    Chore: Added basic authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApartmentsModule } from './apartments/apartments.module';
+import { ApartmentsModule } from './modules/apartments/apartments.module';
 
 @Module({
   imports: [ApartmentsModule],
